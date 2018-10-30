3fdd7e3384b437b74ba3f651fa08e42004932b0e
Júlio Costa <juliojoel@gmail.com>
2018-10-30 12:31:03 -0300

    Implementado método get para ScadaBR

--- teste.py
+++ teste.py
@@ -1,6 +1,8 @@
 import sqlite3
 import obd
 import sys
+import requests as req
+import time
 
 connOBD = obd.OBD(portstr="/dev/ttys003")
 
@@ -16,10 +18,20 @@ def getData():
     rawPos = connOBD.query(cmdPos)
     rawTemp = connOBD.query(cmdTemp)
 
+    print(rawSpeed)
+    print(rawRPM)
+    print(rawPos)
+    print(rawTemp)
+
     resSpeed = rawSpeed.messages[0].data[0]
-    resRPM = rawRPM.messages[0].data[0]
+    resRPM1 = rawRPM.messages[0].data[0]
+    resRPM2 = rawRPM.messages[0].data[1]
     resPos = rawPos.messages[0].data[0]
     resTemp = rawTemp.messages[0].data[0]
+
+    resRPM = (resRPM1 * 256.00 + resRPM2 ) / 4.00
+    resPos = resPos * 100.00 / 255.00
+    resTemp = resTemp - 40.00
     
     return resSpeed, resRPM, resPos, resTemp
 
@@ -27,19 +39,23 @@ def logData (speed, rpm, pos, temp):
     try:
         connDB = sqlite3.connect('obdPE.db')
         c = connDB.cursor()
-        c.execute("insert into speed values (datetime('now'),'%f')" % speed)
-        c.execute("insert into rpm values (datetime('now'),'%f')" % rpm)
-        c.execute("insert into position values (datetime('now'),'%f')" % pos)
-        c.execute("insert into temperature values (datetime('now'),'%f')" % temp)
+        c.execute("insert into obd values (datetime('now'),'%f','%f','%f','%f')" % (speed, rpm, pos, temp))
         connDB.commit()
         c.close()
 
         return "ok!"
     except:
-        return "erro!"
+        return "error!"
     
+while True:
 
-speed, rpm, pos, temp = getData()
+    speed, rpm, pos, temp = getData()
 
-res2 = logData(speed, rpm, pos, temp)
-print(res2)
+    res2 = logData(speed, rpm, pos, temp)
+    print(res2)
+
+    payload = {'obd.speed': speed, 'obd.rpm': rpm, 'obd.pos': pos, 'obd.temp': temp}
+    print(payload)
+    resp = req.get("http://150.162.6.111/ScadaBRy/httpds", params=payload)
+    print(resp)
+    time.sleep(5)
